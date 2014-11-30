a6a581aab67fc5153434630346b0ea5ad6193f81
Daniel Cormier <daniel.cormier+git@gmail.com>
2014-11-30 16:48:20 -0500

    Changed method of checking if device is available.
    Now it checks if we found an enabled "add to cart" button, rather than clicking blindly and hoping it worked, checking if we arrived at our cart after our refresh interval.
    
    Because of that, it now calls PushBullet quicker than it did before.

--- iwantnexus.js
+++ iwantnexus.js
@@ -1,34 +1,43 @@
-var currenturl = window.location.href;
-var iwantit = jQuery('#body-content > div.details-wrapper.devices.id-track-partial-impression.vs-enabled > div.details-info-wrapper > div > div.info-container > div.details-actions > span.buy-button-container > button');
-iwantit.click();
+$(function() {
+	var deviceurl = window.location.href;
+	var add_to_cart = $('#body-content > div.details-wrapper.devices.id-track-partial-impression.vs-enabled > div.details-info-wrapper > div > div.info-container > div.details-actions > span.buy-button-container > button');
 
-chrome.storage.sync.get({
-	refresh_interval:1000
-}, function(items) {
-	setTimeout(function() { 
-		if(window.location.pathname == "/store/cart") {
-			console.log('The device is available.');
-			jQuery('#hardware-checkout').click();
-			//document.getElementById("result").insertAdjacentHTML("beforeEnd", JSON.stringify(res, null, 4) + "\n");
-			chrome.storage.sync.get({
+	if (add_to_cart.is(':enabled')) {
+		add_to_cart.click();
+
+		chrome.storage.sync.get(
+			{
 				api_key: '',
-				devices:[]
-			}, function(items) {
+				devices: []
+			},
+			function(items) {
+				console.log('The device is available.');
+
 				PushBullet.APIKey = items.api_key;
 				console.log('Pushing to ' + items.devices.length + ' devices');
 
-				$.each(items.devices, function(index, device_iden){
-					console.log('Pushing to device ' + index +': ' + device_iden);
-					PushBullet.push("link", device_iden, null, {title: "Device available!", url: currenturl});
+				$.each(items.devices, function(index, device_iden) {
+					console.log('Pushing to device ' + index + ': ' + device_iden);
+					PushBullet.push("link", device_iden, null, { title: "Device available!", url: deviceurl });
 				});
-			});
-		} else {
-			window.location.reload();
-		}
-	}, items.refresh_interval);
+
+				$('#hardware-checkout').click();
+			}
+		);
+	}
+	else {
+		chrome.storage.sync.get(
+			{
+				refresh_interval: 1000
+			},
+			function(items) {
+				setTimeout(
+					function() {
+						window.location.reload();
+					},
+					items.refresh_interval
+				);
+			}
+		);
+	}
 });
-
-function send_pushes()
-{
-
-}
